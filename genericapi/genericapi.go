c543c6429b730732a22344d925152fd2d94fe97c
Andre Soares <andregsilv@gmail.com>
2020-07-31 16:03:01 -0300

    genericapi and mockserver

---
+++ genericapi/genericapi.go
@@ -0,0 +1,90 @@
+package genericapi
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/BraspagDevelopers/canned-testcontainers"
+	"github.com/docker/go-connections/nat"
+	"github.com/palantir/stacktrace"
+	"github.com/pkg/errors"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+const (
+	defaultPort             = "80/tcp"
+	defaultLivenessEndpoint = "/"
+)
+
+type ContainerRequest struct {
+	testcontainers.GenericContainerRequest
+	Image            string
+	LivenessEndpoint string
+	Port             nat.Port
+}
+type Container struct {
+	Container testcontainers.Container
+	req       ContainerRequest
+}
+
+func (req ContainerRequest) WithNetworkAlias(network, alias string) ContainerRequest {
+	canned.AddNetworkAlias(&req.GenericContainerRequest, network, alias)
+	return req
+}
+
+func (c Container) BaseURL(ctx context.Context) (string, error) {
+	host, err := c.Container.Host(ctx)
+	if err != nil {
+		return "", stacktrace.Propagate(err, "Error reading container host name")
+	}
+	port, err := c.Container.MappedPort(ctx, c.req.Port)
+	if err != nil {
+		return "", stacktrace.Propagate(err, "Error reading container mapped port")
+	}
+	return fmt.Sprintf("http://%s:%s", host, port.Port()), nil
+}
+
+func New(ctx context.Context, req ContainerRequest) (*Container, error) {
+	if req.Port == "" {
+		req.Port = defaultPort
+	}
+	if req.Image == "" {
+		return nil, errors.New("an image name is required")
+	}
+	req.GenericContainerRequest.Image = req.Image
+	if req.ExposedPorts == nil {
+		req.ExposedPorts = []string{string(req.Port)}
+	}
+	if req.LivenessEndpoint == "" {
+		req.LivenessEndpoint = defaultLivenessEndpoint
+	}
+	if req.WaitingFor == nil {
+		req.WaitingFor = wait.ForHTTP(req.LivenessEndpoint).
+			WithPort(req.Port)
+	} else {
+		req.WaitingFor = wait.ForAll(
+			req.WaitingFor,
+			wait.ForHTTP(req.LivenessEndpoint).
+				WithPort(req.Port))
+	}
+
+	provider, err := req.ProviderType.GetProvider()
+	if err != nil {
+		return nil, err
+	}
+
+	result := &Container{
+		req: req,
+	}
+
+	if result.Container, err = provider.CreateContainer(ctx, req.ContainerRequest); err != nil {
+		return result, errors.Wrap(err, "Error creating container")
+	}
+
+	if err = result.Container.Start(ctx); err != nil {
+		return result, errors.Wrap(err, "Error starting container")
+	}
+
+	return result, nil
+}
---
+++ mockserver/mockserver.go
@@ -0,0 +1,37 @@
+package mockserver
+
+import (
+	"context"
+
+	"github.com/BraspagDevelopers/canned-testcontainers"
+	"github.com/BraspagDevelopers/canned-testcontainers/genericapi"
+)
+
+const (
+	image            = "mockserver/mockserver"
+	exposedPort      = "1080/tcp"
+	livenessEndpoint = "/_meta/alive"
+)
+
+type ContainerRequest genericapi.ContainerRequest
+type Container genericapi.Container
+
+func (req ContainerRequest) WithNetworkAlias(network, alias string) ContainerRequest {
+	canned.AddNetworkAlias(&req.GenericContainerRequest, network, alias)
+	return req
+}
+
+func New(ctx context.Context, req ContainerRequest) (*Container, error) {
+	if req.Port == "" {
+		req.Port = exposedPort
+	}
+	if req.Image == "" {
+		req.Image = image
+	}
+	if req.LivenessEndpoint == "" {
+		req.LivenessEndpoint = livenessEndpoint
+	}
+	apic, err := genericapi.New(ctx, genericapi.ContainerRequest(req))
+	c := Container(*apic)
+	return &c, err
+}
