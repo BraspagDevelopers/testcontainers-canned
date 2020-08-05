2fd41610258a24a718273e969c410ea48b8cc9a6
Andre Soares <andregsilv@gmail.com>
2020-08-05 10:44:10 -0300

    add comments

--- genericapi/genericapi.go
+++ genericapi/genericapi.go
@@ -16,47 +16,27 @@ const (
 	defaultLivenessEndpoint = "/"
 )
 
+// ContainerRequest a container request specification
 type ContainerRequest struct {
 	testcontainers.GenericContainerRequest
 	Image            string
 	LivenessEndpoint string
 	Port             nat.Port
 }
+
+// Container represents a mock-server container
 type Container struct {
 	Container testcontainers.Container
 	req       ContainerRequest
 }
 
+// WithNetworkAlias adds a network alias to the container request
 func (req ContainerRequest) WithNetworkAlias(network, alias string) ContainerRequest {
 	canned.AddNetworkAlias(&req.GenericContainerRequest, network, alias)
 	return req
 }
 
-func (c Container) URL(ctx context.Context) (string, error) {
-	host, err := c.Container.Host(ctx)
-	if err != nil {
-		return "", errors.Wrap(err, "Error reading container host name")
-	}
-	port, err := c.Container.MappedPort(ctx, c.req.Port)
-	if err != nil {
-		return "", errors.Wrap(err, "Error reading container mapped port")
-	}
-	return fmt.Sprintf("http://%s:%s", host, port.Port()), nil
-}
-
-func (c Container) URLForNetwork(ctx context.Context, network string) (string, error) {
-
-	alias, err := canned.GetAliasForNetwork(ctx, c.req.GenericContainerRequest, network)
-	if err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("http://%s:%s", alias, c.req.Port.Port()), nil
-}
-
-func (c Container) HostAndPort(ctx context.Context) (string, nat.Port, error) {
-	return canned.GetHostAndPort(ctx, c.Container, c.req.Port)
-}
-
+// CreateContainer creates and starts a container and assumes it contains an HTTP API
 func CreateContainer(ctx context.Context, req ContainerRequest) (*Container, error) {
 	if req.Port == "" {
 		req.Port = defaultPort
@@ -100,4 +80,32 @@ func CreateContainer(ctx context.Context, req ContainerRequest) (*Container, err
 	}
 
 	return result, nil
+}
+
+// URL builds an URL that can be used to interact with the container's HTTP API
+func (c Container) URL(ctx context.Context) (string, error) {
+	host, err := c.Container.Host(ctx)
+	if err != nil {
+		return "", errors.Wrap(err, "Error reading container host name")
+	}
+	port, err := c.Container.MappedPort(ctx, c.req.Port)
+	if err != nil {
+		return "", errors.Wrap(err, "Error reading container mapped port")
+	}
+	return fmt.Sprintf("http://%s:%s", host, port.Port()), nil
+}
+
+// URLForNetwork builds an URL that can be used to interact with the container's HTTP API inside the specified network
+func (c Container) URLForNetwork(ctx context.Context, network string) (string, error) {
+
+	alias, err := canned.GetAliasForNetwork(ctx, c.req.GenericContainerRequest, network)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("http://%s:%s", alias, c.req.Port.Port()), nil
+}
+
+// HostAndPort the host and port that can be used to interact with the container's HTTP API
+func (c Container) HostAndPort(ctx context.Context) (string, nat.Port, error) {
+	return canned.GetHostAndPort(ctx, c.Container, c.req.Port)
 }
